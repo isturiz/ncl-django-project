fc860aadbb6a04f4daba39fe6b96a5b5ac9571bb
isturiz <isturizmauricio@gmail.com>
2023-10-02 10:38:38 -0400

    remove flowbite cdn

--- core/settings.py
+++ core/settings.py
@@ -125,9 +125,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
-
+STATIC_ROOT = 'dist/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
+    ('node_modules', os.path.join(BASE_DIR, 'node_modules/')),
 ]
 
 
