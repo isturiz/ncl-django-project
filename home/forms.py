bdaa9e31f0587f2d16e8c524bbbe46b770d27c71
isturiz <isturizmauricio@gmail.com>
2023-10-03 12:27:36 -0400

    fix broken form styles

--- home/forms.py
+++ home/forms.py
@@ -15,7 +15,7 @@ class StudentForm(forms.ModelForm):
             'phone_number': 'Número de Teléfono',
             'address': 'Dirección',
             'birthdate': 'Fecha de Nacimiento',
-            'is_active': 'Estado'
+            'is_active': 'Estudiante activo'
         }
 
     def __init__(self, *args, **kwargs):
@@ -28,7 +28,8 @@ class StudentForm(forms.ModelForm):
         self.fields['phone_number'].widget.attrs.update({'class': 'block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer', 'placeholder': ' '})
         self.fields['address'].widget.attrs.update({'class': 'block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer', 'placeholder': ' '})
         self.fields['birthdate'].widget.attrs.update({'class': 'block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer', 'placeholder': ' ', 'datepicker': '', 'datepicker-orientation': 'bottom left', 'type': 'text', 'autocomplete': 'off'})
-        
+        self.fields['is_active'].widget.attrs.update({'class': 'w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600'})
+
         # # Agregar clases de Tailwind CSS a los widgets de los campos
         # self.fields['first_name'].widget.attrs['class'] = 'block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer'
         # # self.fields['first_name'].label = 'Nombre' 
