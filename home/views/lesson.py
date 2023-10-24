1768f8e99e3be312bd09e32fca4a927ac0741f8e
isturiz <isturizmauricio@gmail.com>
2023-10-24 17:05:49 -0400

    refactor load_subscriptions

--- home/views/lesson.py
+++ home/views/lesson.py
@@ -27,37 +27,11 @@ class LessonUpdateView(UpdateView):
 
 
 def load_subscriptions(request, student_id):
-    print('-------------------------', student_id)
     student = Student.objects.get(pk=student_id)
-    # subscriptions = student.subscription_set.all()  # Asume que el modelo de Student tiene una relación con Subscription
-
-    # subscriptions = Subscription.objects.filter()
     subscriptions = Subscription.objects.filter(student=student)
-    print(subscriptions)
 
     data = {
         'subscriptions': [{'id': sub.student.pk, 'student_name': sub.student.first_name, 'sub_id': sub.pk,'name': sub.subscription_type.name} for sub in subscriptions]
     }
 
     return JsonResponse(data)
-
-class LoadSubscription_View(View):
-    def post(self, request):
-        if request.method == 'POST':
-            event_id = request.POST.get('event_id')
-            new_start_date = request.POST.get('new_start_date')
-            new_end_date = request.POST.get('new_end_date')
-
-            try:
-                event = Lesson.objects.get(id=event_id)
-                event.start_date = new_start_date
-                event.end_date = new_end_date
-                event.save()
-                return JsonResponse({'status': 'success'})
-            except Lesson.DoesNotExist:
-                return JsonResponse({'status': 'error', 'message': 'Evento no encontrado'}, status=404)
-            except Exception as e:
-                return JsonResponse({'status': 'error', 'message': str(e)}, status=400)
-        else:
-            # Si la solicitud no es POST, puedes devolver un error 405 (Method Not Allowed)
-            return JsonResponse({'status': 'error', 'message': 'Método no permitido'}, status=405)
